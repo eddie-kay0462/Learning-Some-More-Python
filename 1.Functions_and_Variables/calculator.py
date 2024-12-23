31ef702c8863390f4a9ace377679a773b276e502
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-12-23 19:35:38 +0000

    Added the addition calculation feature

--- 1.Functions_and_Variables/calculator.py
+++ 1.Functions_and_Variables/calculator.py
@@ -14,6 +14,6 @@ y = float(input("Whats y: "))
 #z= round(x / y, 2) #this is a division operation. It will return a float. The round function is used to round the result to 2 decimal places.
 
 #we can also use the .2f to format the result to 2 decimal places.
-z =x / y
-print(f"{z:.2f}")
+z = x / y
 
+print(f"{z:.2f}")
