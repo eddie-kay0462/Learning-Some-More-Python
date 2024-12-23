af799e77c349882de9f5222aec0297d0f4ef87eb
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-12-23 02:16:11 +0000

    A simple calculator demonstrating some basic math python concepts:

---
+++ 1.Functions_and_Variables/calculator.py
@@ -0,0 +1,19 @@
+# x = int(input("Whats x: "))
+# y = int(input("Whats y: "))
+
+x = float(input("Whats x: "))
+y = float(input("Whats y: "))
+
+# z = round(x + y)
+
+
+
+# print(f"{z:,}") #this is a formatted string, it is a string that has a format specifier. The format specifier is a colon followed by a comma. The comma is a comma separator.
+
+
+#z= round(x / y, 2) #this is a division operation. It will return a float. The round function is used to round the result to 2 decimal places.
+
+#we can also use the .2f to format the result to 2 decimal places.
+z =x / y
+print(f"{z:.2f}")
+
