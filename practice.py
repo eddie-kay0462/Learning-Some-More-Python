260061aeccf51d64b07a910774ddd39210c2e396
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-12-25 15:19:44 +0000

    Practice with merging dictionaries using three distinct approaches

---
+++ practice.py
@@ -0,0 +1,28 @@
+##Dictionaries Practice
+
+#first dictionary
+name_school = {
+    "Edward": "Harvard",
+    "John": "Yale",
+    "Sam": "Princeton"
+
+}
+
+#second dictionary
+name_school2 = {
+    "William": "Ashesi",
+    "Eli": "MIT",
+    "Kofi": "Yale"
+}
+
+#merging dictionaries using update
+# name_school.update(name_school2)
+# print(name_school)
+
+#merging dictionaries using the pipe operator
+# merged_dict = name_school | name_school2
+# print(merged_dict)
+
+#merging dictionaries using the double star operator
+# merged_dict = {**name_school, **name_school2}
+# print(merged_dict)
