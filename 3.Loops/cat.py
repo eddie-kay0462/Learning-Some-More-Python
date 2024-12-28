4d77c2e91c3565968c0e4019b1d2da685713159b
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2024-12-28 00:21:01 +0000

    Practice with loops

---
+++ 3.Loops/cat.py
@@ -0,0 +1,61 @@
+#print meow
+# print("meow")
+# print("meow")
+# print("meow")
+
+#using a while loop
+# i = 0
+# while i < 3:
+#     print(i+1, "meow")
+#     i += 1
+
+
+# i = 0
+# while i < 3:
+#     print(i+1, "meow")
+#     i += 1
+
+# for i in [0,1,2]:
+#     print(i+1, "meow!")
+
+
+# for i in range(100):
+#     print(i+1, "meow!!")
+
+
+# for _ in range(100):
+#     print(_+1, "moeww!")
+
+
+# print("meow\n" * 3, end="")
+
+
+# while True:
+#     n = int(input("What's n? "))
+#     if n > 0:
+#         break
+
+# for _ in range(n):
+#     print("meow")
+
+
+# def main():
+#     n = get_number()
+#     meow(n)
+
+
+# def get_number():
+#     while True:
+#         n = int(input("What's n? "))
+#         if n > 0:
+#             return n
+#         else:
+#             print("n must be positive")
+
+# def meow(n):
+#     for _ in range(n):
+#         print("meow!!!")
+
+# main()
+
+
