a34d460fd64c675264c3d8be0f4e7803f336995c
eddie-kay0462 <edward.mensah@ashesi.edu.gh>
2025-01-02 21:30:24 +0000

    Practice with libraries using statistics module in python

---
+++ 5.Libraries/average.py
@@ -0,0 +1,18 @@
+import statistics
+
+#mean is the average of the numbers in the list
+print(statistics.mean([100, 50]))
+
+#median is the middle number in the list
+print(statistics.median([1, 2, 3, 4, 5]))
+
+#mode is the number that appears most frequently in the list
+print(statistics.mode([1, 2, 2, 3, 3, 3, 4, 4, 4, 4]))
+
+#stdev is the standard deviation of the numbers in the list
+print(statistics.stdev([1, 2, 3, 4, 5]))
+
+#variance is the average of the squared differences from the mean
+print(statistics.variance([1, 2, 3, 4, 5]))
+
+
